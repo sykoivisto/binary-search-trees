82d8a60ea348fc740b42ee0188bc738db4dfc2ae
sy <sykoivisto@gmail.com>
2022-12-03 18:19:50 -0700

    depth first traversal methods

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import Tree from "./tree.js";
+import Node from "./node.js";
 
 let tree = Tree([1, 43, 35, 5, 62, 3, 33, 5, 3, 34, 5, 56, 5]);
-
-tree.insert(2)
 
 tree.prettyPrint();
--- tree.js
+++ tree.js
@@ -1,4 +1,4 @@
-import Node from "./node.js";
+import Node from './node.js';
 
 const Tree = (array) => {
   let root = null; // root of the tree
@@ -25,10 +25,14 @@ const Tree = (array) => {
     return root;
   };
 
+  // set the root of the tree.
+  // this calls the actual building of the tree.
   root = buildTree(array);
 
+  // navigates the tree from the root until finding an appropriate place at the bottom
+  // inserts a new node here.
   const insert = (value, currentNode = root) => {
-    if (currentNode === null) return Node(value, null, null)
+    if (currentNode === null) return Node(value, null, null);
     if (currentNode.data === value) return;
 
     if (currentNode.data < value) {
@@ -39,14 +43,81 @@ const Tree = (array) => {
     return currentNode;
   };
 
-
-  const prettyPrint = (node = root, prefix = "", isLeft = true) => {
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  // accepts any value and returns the node with this value
+  const find = (value, currentNode = root) => {
+    if (currentNode === null) return null;
+    if (currentNode.data === value) {
+      return currentNode;
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+
+    if (currentNode.data < value) {
+      return find(value, currentNode.right);
+    } else {
+      return find(value, currentNode.left);
+    }
+  };
+
+  const levelOrder = (callback) => {
+    const queue = [root];
+    const valueArr = [];
+
+    // while theres things in the queue
+    while (queue.length > 0) {
+      // next in queue
+      const currentNode = queue.shift();
+
+      // provide the node to the callback
+      // if no callback, push to the array of values
+      if (callback) {
+        callback(currentNode);
+      } else {
+        if (currentNode.data) valueArr.push(currentNode.data);
+      }
+
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+    }
+
+    return valueArr;
+  };
+
+  const inorder = (currentNode = root, list = []) => {
+    if (currentNode === null) return;
+
+    if (currentNode.left) inorder(currentNode.left, list);
+    if (currentNode.data) list.push(currentNode.data);
+    if (currentNode.right) inorder(currentNode.right, list);
+
+    if (list.length > 0) return list;
+  };
+
+  const preorder = (currentNode = root, list = []) => {
+    if (currentNode === null) return;
+
+    if (currentNode.data) list.push(currentNode.data);
+    if (currentNode.left) preorder(currentNode.left, list);
+    if (currentNode.right) preorder(currentNode.right, list);
+
+    if (list.length > 0) return list;
+  };
+
+  const postorder = (currentNode = root, list = []) => {
+    if (currentNode === null) return;
+
+    if (currentNode.left) postorder(currentNode.left, list);
+    if (currentNode.right) postorder(currentNode.right, list);
+    if (currentNode.data) list.push(currentNode.data);
+
+    if (list.length > 0) return list;
+  };
+
+  const prettyPrint = (node = root, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
     if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
   };
 
@@ -54,6 +125,11 @@ const Tree = (array) => {
     root,
     prettyPrint,
     insert,
+    find,
+    levelOrder,
+    inorder,
+    preorder,
+    postorder,
   };
 };
 
